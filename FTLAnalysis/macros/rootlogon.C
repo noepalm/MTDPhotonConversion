094ff4fb3c960665acbf9f42bb7a6697f0d8ddeb
noepalm <palmeri.1871005@studenti.uniroma1.it>
2025-04-03 11:18:44 +0200

    Removed unused files

--- FTLAnalysis/macros/rootlogon.C
+++
@@ -1,4 +0,0 @@
-{
-    gROOT->Macro("$CMSSW_BASE/src/ExternalTools/FuriousPlotter/scripts/macros/setStyle.C");
-    gROOT->LoadMacro("$CMSSW_BASE/src/ExternalTools/FuriousPlotter/scripts/macros/FPCanvasStyle.C");
-}
